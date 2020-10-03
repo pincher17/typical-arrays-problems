ef0236535cf24925dbf3128c3091e4de0ca1e8d8
pincher <pinchers17@mail.ru>
2020-10-03 21:38:04 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array) && array.length > 0){
+    return Math.min.apply(null, array);
+  }else{
+    return 0;
+  }
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(Array.isArray(array) && array.length > 0){
+    return Math.max.apply(null, array);
+  }else{
+    return 0;
+  }
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if(Array.isArray(array) && array.length > 0){
+    let sum = 0;
+
+    for(let i = 0; i < array.length; i++){
+     
+       sum += array[i];
+      
+    }
+  
+    let mean = sum/array.length;
+    return mean;
+  }else{
+    return 0;
+  }
+  
+
 }
+
+
+
